c5d88791350e91757ab84189bcd67984d9180d62
CGRem <125661027+CGRem@users.noreply.github.com>
2023-04-30 21:48:40 +0300

    testing gitlink

--- main.cpp
+++ main.cpp
@@ -2,11 +2,14 @@
 #include "objects.h"
 #include "function.h"
 
-//using namespace std;
+using namespace std;
 
 int main()
 {
     setlocale (LC_ALL, "Rus");
+
+    objects_r::St_time rem_St;
+
     system("pause");
     return 0;
 }
--- objects.cpp
+++ objects.cpp
@@ -1,5 +1,7 @@
 #include "objects.h"
 namespace objects_r
 {
-
+    struct St_time{
+        int remtime_Int = 0;
+    };
 }
